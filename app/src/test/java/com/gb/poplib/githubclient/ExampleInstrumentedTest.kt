afde69a7387874209785c897dab40c6d8a9de9f0
AlekseySapi <altmind92@gmail.com>
2023-06-20 20:01:03 +0300

    Changes at build.gradle and ExampleInstrumentedTest

--- app/src/test/java/com/gb/poplib/githubclient/ExampleInstrumentedTest.kt
+++ app/src/test/java/com/gb/poplib/githubclient/ExampleInstrumentedTest.kt
@@ -1,7 +1,5 @@
 package com.gb.poplib.githubclient
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +9,9 @@ import org.junit.runner.RunWith
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+/*
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
@@ -20,3 +21,4 @@ class ExampleInstrumentedTest {
         assertEquals("com.gb.poplib.githubclient", appContext.packageName)
     }
 }
+ */
