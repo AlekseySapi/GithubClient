e7ee8339c1477fe45a8de4099da2a2ae2b032694
AlekseySapi <altmind92@gmail.com>
2023-06-20 19:52:11 +0300

    Changes at Presenter and MainActivity

--- app/src/main/java/com/gb/poplib/githubclient/MainActivity.kt
+++ app/src/main/java/com/gb/poplib/githubclient/MainActivity.kt
@@ -2,36 +2,68 @@ package com.gb.poplib.githubclient
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.gb.poplib.githubclient.databinding.ActivityMainBinding
+
+private const val countersKey = "COUNTERS"
 
 class MainActivity : AppCompatActivity(), MainView {
-    private var vb: ActivityMainBinding? = null
-    val presenter = MainPresenter(this)
 
-    val counters = mutableListOf(0, 0, 0)
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val presenter: MainPresenter = MainPresenter(this)
+    private val counters = mutableListOf<Int>(0, 0, 0)
+
+    private lateinit var countersViewId: List<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        vb = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(vb?.root)
+        countersViewId = listOf(
+            binding.counter1.id,
+            binding.counter2.id,
+            binding.counter3.id
+        )
 
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
+        val clickListener = View.OnClickListener {
+            presenter.onViewClick(it.id, countersViewId)
         }
 
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        binding.counter1.setOnClickListener(clickListener)
+        binding.counter2.setOnClickListener(clickListener)
+        binding.counter3.setOnClickListener(clickListener)
     }
 
-    override fun setButtonText(index: Int, text: String) {
-        when(index) {
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(countersKey, counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val savedCounters = savedInstanceState.getIntArray(countersKey)
+        savedCounters?.toList()?.let {
+            counters.clear()
+            counters.addAll(it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun setButtonText(counterIndex: Int, text: String) {
+        countersViewId[counterIndex].let { viewId ->
+            binding.root.findViewById<Button>(viewId)?.let { view ->
+                view.text = text
+            }
         }
     }
 }
