fee621ad02566276ee0c8702b0ef973cc8c8da87
AlekseySapi <altmind92@gmail.com>
2023-06-27 20:01:11 +0300

    Добавлен вывод данных

--- app/src/main/java/com/gb/poplib/githubclient/ui/IScreens.kt
+++ app/src/main/java/com/gb/poplib/githubclient/ui/IScreens.kt
@@ -1,7 +1,9 @@
 package com.gb.poplib.githubclient.ui
 
+import com.gb.poplib.githubclient.data.GithubUser
 import com.github.terrakok.cicerone.Screen
 
 interface IScreens {
     fun users(): Screen
+    fun user(user: GithubUser?): Screen
 }
